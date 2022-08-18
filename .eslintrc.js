08b35a817524ad240637a2dec62a822f294c9f94
justin <978336184@qq.com>
2022-08-18 17:50:14 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// https://eslint.org/docs/user-guide/configuring/ignoring-code#the-eslintignore-file
+// https://eslint.org/docs/rules/
+// https://github.com/prettier/eslint-plugin-prettier
+
+module.exports = {
+  extends: ['plugin:prettier/recommended', 'react-app', 'react-app/jest']
+};
