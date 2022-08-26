2768078896668faf81c07d4b7e7a244e5a0031ba
justin <978336184@qq.com>
2022-08-26 18:32:47 +0800

    pro antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import routes from '@/routes/index';
 import ChildrenView from '@/routes/childrenView';
+import Layout from '@/components/Layout';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to='/tacos'>Tacos</Link>
-          </li>
-          <li>
-            <Link to='/sandwiches'>Sandwiches</Link>
-          </li>
-        </ul>
-        <ChildrenView routes={routes} />
-      </div>
-    </Router>
+    <Layout />
+    // <Router>
+    //   <div>
+    //     <ul>
+    //       <li>
+    //         <Link to='/tacos'>Tacos</Link>
+    //       </li>
+    //       <li>
+    //         <Link to='/sandwiches'>Sandwiches</Link>
+    //       </li>
+    //     </ul>
+    //     <ChildrenView routes={routes} />
+    //   </div>
+    // </Router>
   );
 };
 
