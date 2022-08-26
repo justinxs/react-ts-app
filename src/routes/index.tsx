dbee14490132dea4a851816ce46aeec64aa5a4e1
justin <978336184@qq.com>
2022-08-26 18:00:23 +0800

    lazy component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,8 @@
+import { Spin } from 'antd';
 import lazyComp from './lazyComp';
+import Cart from '@/pages/Cart';
+
+const Loading: React.FC = () => <Spin size='large' />;
 
 const routes: RouteList = [
   {
@@ -7,15 +11,19 @@ const routes: RouteList = [
   },
   {
     path: '/tacos',
-    component: lazyComp(() => import('../pages/Tacos')),
+    component: lazyComp(() => import('../pages/Tacos'), {
+      fallback: <Loading />
+    }),
     routes: [
       {
         path: '/tacos/bus',
-        component: lazyComp(() => import('../pages/Bus'))
+        component: lazyComp(() => import('../pages/Bus'), {
+          fallback: <Loading />
+        })
       },
       {
         path: '/tacos/cart',
-        component: lazyComp(() => import('../pages/Cart'))
+        component: Cart
       }
     ]
   }
--- src/routes/lazyComp.tsx
+++ src/routes/lazyComp.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import loadable from '@loadable/component';
-import { Spin } from 'antd';
 
-const Loading: React.FC = () => <Spin size='large' />;
-
-export default function lazyComp(CompFn: () => Promise<any>) {
-  const LoadableComponent = loadable(CompFn, {
-    fallback: <Loading />
-  });
+export default function lazyComp(CompFn: () => Promise<any>, options?: any) {
+  const LoadableComponent = loadable(CompFn, options);
   return (props: any) => <LoadableComponent {...props} />;
 }
